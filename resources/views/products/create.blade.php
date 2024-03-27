206ae031083b6f514e0d70c5c33bec2f2ecf048a
akosidonel <reydonelmartinez@gmail.com>
2024-03-27 11:29:26 +0800

    first commit

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,3 +1,10 @@
 <div>
     <h1>create</h1>
+    <form action="" method="">
+    <div>
+        <label for=""></label>
+        <input type="text" name="name" id="name">
+    </div>
+
+    </form>
 </div>
